76f4e036399a5aa318690edc0686218f4074bf32
Sohel440 <109268326+Sohel440@users.noreply.github.com>
2024-08-24 20:42:26 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Common Substring/longest-common-substring.cpp
+++ Difficulty: Medium/Longest Common Substring/longest-common-substring.cpp
@@ -6,42 +6,59 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-  int mx =0;
-    int dp[1001][1001];
-    
-    int solve(int i  , int j  , string &a , string &b){
+    int mx = -1;
+    int solve(int i , int j , string &a , string &b){
         
         if(i >= a.size() || j >= b.size()){
             return 0;
         }
-        if(dp[i][j] != -1) return dp[i][j];
-        
         int ans =0;
         
-        if(a[i]== b[j]){
-           
-           ans =  1 + solve( i +1 , j+1 , a , b);
-           mx = max(mx , ans);
-          //return solve( i +1 , j+1 , a , b);
-          
+        if(a[i] == b[j]){
+            ans++;
+            mx = max(ans  , mx);
+            return solve(i +1 , j +1 , a , b);
         }
-        int take   = solve(i+1 , j, a , b);
-        int nontake = solve(i , j+1 , a, b);
+        
+        int take    = solve(i+1 , j , a  , b);
+        int nontake = solve(i , j+1 , a , b);
         
         
-        return dp[i][j] = ans;
+        return mx;
+        
     }
     int longestCommonSubstr(string a, string b) {
-        
         // your code here
-        memset(dp , -1 , sizeof(dp));
         
-         solve(0  , 0 , a , b);
-         //cout << mx << endl;
-         return mx;
-         
+        int n = a.size();;
+        int m = b.size();
         
+        vector<vector<int>> dp (n+1, vector<int> (m+1 , 0));
+        int mx =0;
+        for(int i = 1 ; i <=n ; i++){
+            for(int j = 1 ;j <= m ; j++){
+                
+                if(a[i-1]==b[j-1]){
+                    dp[i][j]= 1+ dp[i-1][j-1];
+                    mx = max(dp[i][j] , mx);
+                }
+                
+            }
+        }
+        
+        // for(int i =1 ; i<=n ; i++){
+        //     for(int j= 1 ; j <=m ;j++){
+        //         cout << dp[i][j]<< " ";
+        //     }
+        //     cout << endl;
+        // }
+        
+        
+        
+        
+        return mx;
     }
+    
 };
 
 //{ Driver Code Starts.
