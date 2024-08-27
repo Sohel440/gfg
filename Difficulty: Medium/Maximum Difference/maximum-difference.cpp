6212e231d9e868b3700936ef228af83e4e2e48f3
Sohel440 <109268326+Sohel440@users.noreply.github.com>
2024-08-27 21:28:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Maximum Difference/maximum-difference.cpp
@@ -0,0 +1,70 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public: 
+    /*You are required to complete this method */
+   void solve(vector<int> &arr , vector<int> &ls){
+    stack<int> st;
+    for(int i = 0; i < arr.size(); i++){
+        while(!st.empty() && st.top() >= arr[i]){
+            st.pop();
+        }
+        
+        if(!st.empty()){
+            ls[i] = st.top();
+        } else {
+            ls[i] = 0;
+        }
+        
+        st.push(arr[i]);
+    }
+}
+
+    int findMaxDiff(vector<int> &arr) {
+        // Your code here
+        vector<int> ls(arr.size() , 0);
+        solve(arr , ls);
+        vector<int> narr = arr;
+        reverse(begin(narr) , end(narr));
+        vector<int> rs(narr.size(), 0);
+        solve(narr , rs);
+        reverse(begin(rs), end(rs));
+        
+        int mx =0;
+        
+        for(int i = 0 ; i < ls.size(); i++){
+            mx = max(mx , abs(ls[i]- rs[i]));
+        }
+        
+        return mx;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+
+    while (t--) {
+        string input;
+        int num;
+        vector<int> arr;
+
+        getline(cin, input);
+        stringstream s2(input);
+        while (s2 >> num) {
+            arr.push_back(num);
+        }
+
+        Solution ob;
+        cout << ob.findMaxDiff(arr) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
