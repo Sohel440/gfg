1d0e62cb2004d86b721bbfe33a070b96218c0413
Sohel440 <109268326+Sohel440@users.noreply.github.com>
2024-09-12 02:33:37 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Middle of a Linked List/middle-of-a-linked-list.java
@@ -0,0 +1,76 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}
+
+class GFG {
+    static void printList(Node node) {
+        while (node != null) {
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t > 0) {
+            String str[] = read.readLine().trim().split(" ");
+            int n = str.length;
+            Node head = new Node(Integer.parseInt(str[0]));
+            Node tail = head;
+            for (int i = 1; i < n; i++) {
+                tail.next = new Node(Integer.parseInt(str[i]));
+                tail = tail.next;
+            }
+            Solution g = new Solution();
+            int ans = g.getMiddle(head);
+            System.out.println(ans);
+            // printList(head);
+            t--;
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+/* Node of a linked list
+ class Node {
+   int data;
+    Node next;
+    Node(int d)  { data = d;  next = null; }
+}
+*/
+
+class Solution {
+    int getMiddle(Node head) {
+        // Your code here.
+        LinkedList<Integer> list = new LinkedList<>();
+        
+        Node temp = head;
+        while(temp != null){
+            
+            list.add(temp.data);
+            temp = temp.next;
+            
+        }
+        
+        
+        int mid =  list.size()/2;
+       // System.out.println(list);
+        
+        return list.get(mid);
+        
+    }
+}
